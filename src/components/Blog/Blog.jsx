15d703dfb6358b1c0f5bf56749ce0fa6b17ed435
Salman7D <salmanrayhan753@gmail.com>
2024-03-13 21:40:03 +0600

    resonable

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,16 +1,35 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({ blog }) => {
-    console.log(blog);
+    const {title, cover, author_img, author, posted_date, reading_time, hashtag}= blog;
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between mb-4'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+           <h2 className="text-4xl">{title}</h2> 
+           <p>
+            {
+                hashtag.map((hash, idx) => <span key={idx}><a href="">#{hash}</a> </span>)
+            }
+           </p>
         </div>
     );
 };
 
-Blog.PropTypes = {
-    Blog: PropTypes.object.isRequired
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired
 }
 
 export default Blog;
