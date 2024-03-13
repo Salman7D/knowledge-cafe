5525281c560454727666415527031034442725f8
Salman7D <salmanrayhan753@gmail.com>
2024-03-13 23:15:16 +0600

    bookmarks added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 
+import { useState } from 'react'
 import './App.css'
 import Bookmarks from './Bookmarks/Bookmarks'
 import Blogs from './components/Header/Blogs/Blogs'
 import Header from './components/Header/Header'
 
 function App() {
+  const [bookmarks, setBookmarks]= useState([]);
 
+  const handleAddToBookmark = blog =>{
+   const newBookmarks = [...bookmarks, blog];
+   setBookmarks(newBookmarks);
+  }
   return (
     <>
       
       <Header></Header>
       <div className='md:flex max-w-7xl mx-auto'>
-        <Blogs></Blogs>
-        <Bookmarks></Bookmarks>
+        <Blogs handleAddToBookmark = {handleAddToBookmark}></Blogs>
+        <Bookmarks bookmarks={bookmarks}></Bookmarks>
       </div>
     </>
   )
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
+import { FaBookmark } from "react-icons/fa";
 
-const Blog = ({ blog }) => {
+
+const Blog = ({ blog, handleAddToBookmark }) => {
+    // console.log(blog);
     const {title, cover, author_img, author, posted_date, reading_time, hashtag}= blog;
+    console.log(cover);
     return (
         <div className='mb-20'>
             <img className='w-full mb-8' src={cover} alt={`Cover picture of the title ${title}`} />
+            
             <div className='flex justify-between mb-4'>
                 <div className='flex'>
                     <img className='w-14' src={author_img} alt="" />
@@ -16,6 +21,7 @@ const Blog = ({ blog }) => {
 
                 <div>
                     <span>{reading_time} min read</span>
+                    <button onClick={() => handleAddToBookmark(blog)} className='ml-2 text-2xl text-red-600'><FaBookmark></FaBookmark></button>
                 </div>
             </div>
            <h2 className="text-4xl">{title}</h2> 
@@ -29,7 +35,9 @@ const Blog = ({ blog }) => {
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func
+    
 }
 
 export default Blog;
